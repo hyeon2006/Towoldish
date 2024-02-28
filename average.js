9ef93e87a3cedbabbca23982d4db46c8ea5d94f4
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2024-02-29 08:03:25 +0900

    업뎃

--- average.js
+++ average.js
@@ -28,3 +28,23 @@ cron.schedule('0 0 * * *', initializeAverage);
 app.listen(80, () => {
   console.log('Server listening on port 80');
 });
+
+// Redirect to rating.html with the average rating as a query parameter
+location.href = 'rating.html?averageRating=' + averageRating;
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1].replace(/\+/g, ' '));
+    }
+  }
+  return null;
+}
+
+// Get the average rating from the URL
+var averageRating = getQueryVariable('averageRating');
+
+// Display the average rating in your HTML
+document.getElementById('averageRating').innerText = '평균 등급: ' + averageRating;
