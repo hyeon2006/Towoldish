4bb6179495e4a0c17ae45f8c5a3104ffdf8ec44a
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2024-03-14 11:32:42 +0900

    수정

--- server.js
+++ server.js
@@ -1,29 +1,20 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-const bodyParser = require("body-parser");
-const ratingArray = [];
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const serverData = []; // server.js 배열
 
-app.post("/addRating", (req, res) => {
-  const rating = req.body.rating;
-  ratingArray.push(rating);
-  const average = ratingArray.reduce((a, b) => a + b, 0) / ratingArray.length;
-  res.json({ average });
+app.post('/sendRating', (req, res) => {
+    const { rating } = req.body;
+    serverData.push(rating);
+    console.log('Received rating:', rating);
+    console.log('Updated server data:', serverData);
+    res.sendStatus(200);
 });
 
-app.get("/getAverage", (req, res) => {
-  const average = ratingArray.reduce((a, b) => a + b, 0) / ratingArray.length;
-  res.json({ average });
-});
-
-// 매일 자정에 ratingArray 배열 초기화
-const cron = require("node-cron");
-cron.schedule("0 0 * * *", () => {
-  console.log("ratingArray 배열을 초기화합니다.");
-  ratingArray.length = 0;
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
